c1ddfc2ab941610b1b1014926739c90ba784427a
binhlecong <congbinh21012001@gmail.com>
2021-04-30 08:47:09 +0700

    Create a scoket class for the app to use

--- client/src/model.py
+++ client/src/model.py
@@ -13,20 +13,8 @@ Cmd = {"connect": "connect",
        "registry": "registry",
        "exit": "exit"}
 
-
-# HOST = input('> Enter host: ')
-# PORT = input('> Enter port: ')
-
-HOST = '127.0.0.1'
-PORT = 54321
-
-if not PORT:
-    PORT = 54321
-else:
-    PORT = int(PORT)
-
-BUFF_SIZE = 1024
-ADDR = (HOST, PORT)
+# Text-based ommands are sent to the server
+# The server than perform operation with window API accordingly
 
 
 class MySocket:
@@ -37,7 +25,8 @@ class MySocket:
         else:
             self.sock = sock
 
-    def connect(self, address):
+    def connect(self, ip, port=54321):
+        address = (ip, port)
         self.sock.connect(address)
 
     def send(self, command="exit"):
@@ -45,28 +34,34 @@ class MySocket:
         self.sock.sendall(bytes(command, "utf8"))
         print("> request: " + str(command))
 
-    def receive(self):
+    def receive(self, buff_size=2048):
         try:
-            response = self.recv_timeout(self.sock, BUFF_SIZE)
+            response = self.recv_timeout(self.sock, buff_size)
             print("> response received, data size:", sys.getsizeof(response))
             return response
         except OSError:
             return OSError
 
     def close(self):
+        # Close the socket file descriptor
+        # Both sends and receives are disallowed
         self.sock.close()
 
+    def shutdown(self):
+        # Shutdown one halves of the connection
+        # Further sends are disallowed
+        self.sock.shutdown(sk.SHUT_WR)
+
     def recv_timeout(self, the_socket, buff, timeout=2):
-        # Accumulate all chunks of data sent by the server
+        # Collect chunk of data from the server
+        # We know that evrything has been received when THE CONNECTION IS CLOSED
         the_socket.setblocking(0)
         total_data = []
         data = ""
         begin = time.time()
         while True:
-            # if you got some data, then break after wait sec
             if total_data and time.time() - begin > timeout:
                 break
-            # if you got no data at all, wait a little longer
             elif time.time() - begin > timeout * 2:
                 break
             try:
@@ -81,9 +76,15 @@ class MySocket:
         return b"".join(total_data)
 
 
-s = MySocket()
-s.connect(ADDR)
-cmd = input("> choose a command: ")
-s.send(Cmd[cmd])
-print(s.receive().decode("utf8"))
+# test case
+for i in range(3):
+    s = MySocket()
+    s.connect(ip='localhost', port=54321)
+    cmd = input("> choose a command: ")
+    if cmd != "exit":
+        break
+    s.send(Cmd[cmd])
+    s.receive()
+    s.shutdown()
+
 s.close()
