559c359b9c97e20794526f4b65d4636991b77c58
binhlecong <congbinh21012001@gmail.com>
2021-04-30 21:55:14 +0700

    screenshot window can automatically resize img, use attr in Screenshot class to save img data

--- client/main.py
+++ client/main.py
@@ -5,24 +5,28 @@ from PIL import Image, ImageTk
 
 
 def main():
-    file = ImageTk.PhotoImage(Image.open("E:/My_document/MON_MANG_MAY_TINH/Socket-Programming/client/_server_assets/ndtt.jpg"))
+    # file = ImageTk.PhotoImage(Image.open("E:/My_document/MON_MANG_MAY_TINH/Socket-Programming/client/_server_assets/ndtt.jpg"))
     pic = ui.Screenshot(ui.root)
 
     # these two line exist within the while loop of tk.mainloop()
     # So, the question is: how do you execute something over and over again without actually
     # creating an infinite loop? Tkinter has an answer for that problem: a widget's after() method:
-    pic.update_idletasks()
-    pic.update()
+    
 
+    file0 = open("E:/My_document/MON_MANG_MAY_TINH/Socket-Programming/client/_server_assets/menu.png", "rb")
+    file1 = open("E:/My_document/MON_MANG_MAY_TINH/Socket-Programming/client/_server_assets/screenshot.png", "rb")
     time.sleep(1)
-    pic.update_image(file)
+    pic.update_image(file0.read())
+    file0.close()
 
-    file1 = ImageTk.PhotoImage(Image.open("E:/My_document/MON_MANG_MAY_TINH/Socket-Programming/client/_server_assets/menu.png"))
-    time.sleep(1)
-    pic.update_image(file1)
 
-    file = open("E:/My_document/MON_MANG_MAY_TINH/Socket-Programming/client/_server_assets/menu.png", "rb").read()
-    pic.save_image(file)
+    yn = input("switch:")
+    if yn == "y":
+        pic.update_image(file1.read())
+    file1.close()
+
+
+    #pic.save_image()
 
     pic.mainloop()
     # app = menu.Application(menu.root)
--- client/src/view.py
+++ client/src/view.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 import sys
+import io
 from tkinter import messagebox, filedialog
+from PIL import Image, ImageTk
 
 
 LARGE_FONT = ("Verdana", 12)
@@ -10,11 +12,14 @@ SMALL_FONT = ("Helvetica", 8)
 root = tk.Tk()
 
 
-def messagebox(title="client", msg="Done"):
-    popup = tk.Tk()
-    popup.withdraw()
-    tk.messagebox.showinfo(title, msg)
-    popup.destroy()
+def messagebox(title="client", msg="Done", type="info"):
+    # type are: warn, error, info
+    if type == "error":
+        tk.messagebox.showerror(title, msg)
+    elif type == "warn":
+        tk.messagebox.showwarning(title, msg)
+    else:
+        tk.messagebox.showinfo(title, msg)
 
 
 class Menu(tk.Frame):
@@ -32,7 +37,7 @@ class Menu(tk.Frame):
         # Display the entered IP address and connection state
         self.resultLabel = tk.Label(self, text="IP address", width=15)
         self.resultLabel.grid(row=0, column=0, sticky="E",
-                              padx=10, pady=5, columnspan=1)
+                              padx=10, pady=10, columnspan=1)
 
         # Get user input of IP address
         self.myEntry = tk.Entry(self, width=40)
@@ -44,49 +49,49 @@ class Menu(tk.Frame):
         self.btn_connect = tk.Button(self, text="CONNECT", fg="green",
                                      command=self.returnIP)
         self.btn_connect.grid(row=0, column=3, sticky=tk.W +
-                              tk.S+tk.E+tk.N, padx=10, pady=5, columnspan=1)
+                              tk.S+tk.E+tk.N, padx=10, pady=10, columnspan=1)
 
         # Show running processes
         self.btn_process = tk.Button(self, text="Process running",
                                      command=None, width=30)
         self.btn_process.grid(row=1, column=0, sticky=tk.W+tk.S +
-                              tk.E+tk.N, padx=10, pady=5, columnspan=2, rowspan=1)
+                              tk.E+tk.N, padx=10, pady=10, columnspan=2, rowspan=1)
 
         # Show running apps
         self.btn_app = tk.Button(self, text="App running",
                                  command=None, width=30)
         self.btn_app.grid(row=1, column=2, sticky=tk.W+tk.S +
-                          tk.E+tk.N, padx=10, pady=5, columnspan=2, rowspan=1)
+                          tk.E+tk.N, padx=10, pady=10, columnspan=2, rowspan=1)
 
         # Shutdown computer
         self.btn_shutdown = tk.Button(self, text="Shut down",
                                       command=None)
         self.btn_shutdown.grid(
-            row=2, column=0, sticky=tk.W+tk.S+tk.E+tk.N, padx=10, pady=5, columnspan=2)
+            row=2, column=0, sticky=tk.W+tk.S+tk.E+tk.N, padx=10, pady=10, columnspan=2)
 
         # Take screenshot
         self.btn_screenshot = tk.Button(self, text="Take screenshot",
                                         command=None)
         self.btn_screenshot.grid(
-            row=2, column=2, sticky=tk.W+tk.S+tk.E+tk.N, padx=10, pady=5, columnspan=2)
+            row=2, column=2, sticky=tk.W+tk.S+tk.E+tk.N, padx=10, pady=10, columnspan=2)
 
         # Get keystroke
         self.btn_keystroke = tk.Button(self, text="Get keystroke",
                                        command=None)
         self.btn_keystroke.grid(
-            row=3, column=0, sticky=tk.W+tk.S+tk.E+tk.N, padx=10, pady=5, columnspan=2)
+            row=3, column=0, sticky=tk.W+tk.S+tk.E+tk.N, padx=10, pady=10, columnspan=2)
 
         # Change registry
         self.btn_registry = tk.Button(self, text="Change registry",
                                       command=None)
         self.btn_registry.grid(
-            row=3, column=2, sticky=tk.W+tk.S+tk.E+tk.N, padx=10, pady=5, columnspan=2)
+            row=3, column=2, sticky=tk.W+tk.S+tk.E+tk.N, padx=10, pady=10, columnspan=2)
 
         # Exit program
         self.btn_quit = tk.Button(self, text="EXIT", fg="red",
                                   command=self.master.destroy)
         self.btn_quit.grid(row=4, column=1, sticky=tk.W +
-                           tk.S+tk.E+tk.N, padx=10, pady=5, columnspan=2)
+                           tk.S+tk.E+tk.N, padx=10, pady=10, columnspan=2)
 
     def returnIP(self, args=None):
         print(args)
@@ -99,8 +104,8 @@ class Screenshot(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
         self.master = master
-        self.master.title("Picture")
-        # self.master.geometry("640x560+250+150")
+        self.master.title("Screenshot")
+        self._image_bytes = None
         self.master.grid_columnconfigure(0, weight=1)
         self.master.grid_rowconfigure(0, weight=1)
         self.grid()
@@ -108,35 +113,62 @@ class Screenshot(tk.Frame):
 
     def create_widgets(self):
         # Display the image
-        self.canvas = tk.Canvas(self, width=560, height=480)
+        self.canvas = tk.Canvas(self, width=560, height=560)
         self.item_on_canvas = self.canvas.create_image(
             5, 5, anchor=tk.NW, image=None)
         self.canvas.grid(row=0, column=0, sticky=tk.W +
-                         tk.N, padx=10, pady=5, rowspan=2)
+                         tk.N, padx=10, pady=10, rowspan=2)
 
         # Take another screenshot and update the picture
         # When pressed, Screenshot will sent a request to the server via Controller
         self.btn_connect = tk.Button(
             self, text="Chụp", command=None, width=20, height=20)
-        self.btn_connect.grid(row=0, column=1, sticky=tk.E, padx=10, pady=5)
+        self.btn_connect.grid(row=0, column=1, sticky=tk.E, padx=10, pady=10)
 
         # Write the picture to a file as .png, .jpg and .bmp
         self.btn_process = tk.Button(
-            self, text="Lưu", command=self.save_image, width=20, height=20)
-        self.btn_process.grid(row=1, column=1, sticky=tk.E, padx=10, pady=5)
+            self, text="Lưu", command=lambda: self.save_image(self.image_bytes), width=20, height=20)
+        self.btn_process.grid(row=1, column=1, sticky=tk.E, padx=10, pady=10)
+
+    def _resize_image(self, IMG):
+        h = w = 0
+
+        basewidth = 560
+        wpercent = basewidth / float(IMG.size[0])
+        hsize = int((float(IMG.size[1]) * float(wpercent)))
+
+        if (hsize > 560):
+            h = 560
+            hpercent = h / float(hsize)
+            w = int((float(basewidth) * float(hpercent)))
+        else:
+            h = hsize
+            w = basewidth
+
+        return IMG.resize((int(w), int(h)), Image.ANTIALIAS)
 
     def update_image(self, img_data):
         # img_data: bytes
         # Take image data in bytes from and update the image in the canvas
-        self.canvas.itemconfig(self.item_on_canvas, image=img_data)
+        self._image_bytes = img_data
+        stream = io.BytesIO(img_data)
+        self._img = ImageTk.PhotoImage(self._resize_image(Image.open(stream)))
+        self.canvas.itemconfig(self.item_on_canvas, image=self._img)
         self.update_idletasks()
         self.update()
 
-    def save_image(self, img_data):
+    def save_image(self, img_data=None):
         # img_data: bytes
         # Save an image in bytes form
+        if img_data == None:
+            img_data = self._image_bytes
+            if img_data == None:
+                messagebox("Screenshot", "Image data corrupted", "error")
+                exit
         files = [('PNG', '*.png'),
                  ('JPEG', '*.jpg;*.jpeg'),
                  ('Bitmap', '*.bmp')]
-        file = filedialog.asksaveasfile(mode="wb", filetypes=files, defaultextension=files,title="Save image")
-        file.write(img_data)
+        file = filedialog.asksaveasfile(
+            mode="wb", filetypes=files, defaultextension=files, title="Save image")
+        if file != None:
+            file.write(img_data)
