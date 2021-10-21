27acb4e9571cb4e6b545289c1a6708bb260e885f
Nguyễn Lê Bảo Thi <66853454+baothi250501@users.noreply.github.com>
2021-10-21 14:00:50 +0700

    Update Keystroke GUI + Manager GUI

--- code/client/src/frames/keystroke.py
+++ code/client/src/frames/keystroke.py
@@ -1,4 +1,5 @@
 from tkinter import ttk
+from tkinter.constants import HORIZONTAL, VERTICAL
 from src.mysocket import MySocket
 import tkinter as tk
 import src.themecolors as THEMECOLOR
@@ -10,40 +11,66 @@ class Keystroke(tk.Frame):
     LOCK_BTN_LABEL = 'Khóa'
     UNLOCK_BTN_LABEL = 'Mở khóa'
 
+
+
     def __init__(self, parent):
         tk.Frame.__init__(self, parent, bg=THEMECOLOR.body_bg)
         self.create_widgets()
         self._socket = MySocket.getInstance()
 
+
+
     def clean_activity(self):
         pass
+
     
+
     def create_widgets(self):
+        self.vertical_pane = ttk.PanedWindow(self, orient=VERTICAL, height = 720)
+        self.vertical_pane.grid(row=0, column=0, sticky="nsew")
+        self.horizontal_pane = ttk.PanedWindow(self.vertical_pane, orient=HORIZONTAL, width = 1024)
+        self.vertical_pane.add(self.horizontal_pane)
+        self.button_frame = ttk.Labelframe(self.horizontal_pane, text="My Button")
+        self.button_frame.columnconfigure(1, weight=2)
+        self.horizontal_pane.add(self.button_frame, weight=1)
+        self.console_frame = ttk.Labelframe(self.horizontal_pane, text="Console")
+        self.console_frame.columnconfigure(0, weight=3)
+        self.console_frame.rowconfigure(0, weight=1)
+        self.horizontal_pane.add(self.console_frame, weight=1)
+
+
+
         # Start collecting keystroke fro the server
         self.btn_hook = tk.Button(
-            self, text=Keystroke.HOOK_BTN_LABEL, command=self.keystroke_hook, width=10, height=2)
-        self.btn_hook.grid(row=0, column=5, sticky=tk.N,
+            self.button_frame, text=Keystroke.HOOK_BTN_LABEL, command=self.keystroke_hook, width=10, height=2)
+        self.btn_hook.grid(row=0, column=0, sticky=tk.N,
                            padx=10, pady=5, rowspan=2)
+
         # Print the collected keystrokes
         self.btn_print = tk.Button(
-            self, text="In phím", command=self.keystroke_print, width=10, height=2)
-        self.btn_print.grid(row=2, column=5, sticky=tk.N,
+            self.button_frame, text="In phím", command=self.keystroke_print, width=10, height=2)
+        self.btn_print.grid(row=2, column=0, sticky=tk.N,
                             padx=10, pady=5, rowspan=2)
+
         # Clear the screen
+
         self.btn_clear = tk.Button(
-            self, text="Xóa", command=self.keystroke_clear, width=10, height=2)
-        self.btn_clear.grid(row=4, column=5, sticky=tk.N,
+            self.button_frame, text="Xóa", command=self.keystroke_clear, width=10, height=2)
+        self.btn_clear.grid(row=4, column=0, sticky=tk.N,
                             padx=10, pady=5, rowspan=2)
+
         # Lock the keyboard
         self.btn_lock = tk.Button(
-            self, text=Keystroke.LOCK_BTN_LABEL, command=self.keystroke_lock, width=10, height=2)
-        self.btn_lock.grid(row=6, column=5, sticky=tk.N,
+            self.button_frame, text=Keystroke.LOCK_BTN_LABEL, command=self.keystroke_lock, width=10, height=2)
+        self.btn_lock.grid(row=6, column=0, sticky=tk.N,
                            padx=10, pady=5, rowspan=2)
+
         # Display the text
         self.text_field = tk.Text(
-            self, width=64, height=20, bg="#FFFFFF", state="disable")
-        self.text_field.grid(row=0, column=0, sticky=tk.N,
-                             padx=10, pady=5, columnspan=5, rowspan=8)
+            self.console_frame, width=64, height=20, bg="#FFFFFF", state="disable")
+        self.text_field.grid(row=0, column=0, sticky=tk.N + tk.W + tk.E + tk.S)
+
+
 
     def keystroke_lock(self):
         if self.btn_lock.cget('text') == Keystroke.LOCK_BTN_LABEL:
@@ -53,11 +80,15 @@ class Keystroke(tk.Frame):
             self._socket.send_immediate('keystroke,unlock')
             self.btn_lock.configure(text=Keystroke.LOCK_BTN_LABEL)
 
+
+
     def print_keystroke(self, keystroke="<Not hooked>"):
         keystroke = keystroke + "\n"
         self.text_field.configure(state="normal")
         self.text_field.insert("end", keystroke)
         self.text_field.configure(state="disable")
+
+
 
     def keystroke_hook(self):
         if self.btn_hook.cget('text') == Keystroke.HOOK_BTN_LABEL:
@@ -67,12 +98,17 @@ class Keystroke(tk.Frame):
             self._socket.send_immediate('keystroke,unhook')
             self.btn_hook.configure(text=Keystroke.HOOK_BTN_LABEL)
 
+
+
     def keystroke_print(self):
         self._socket.send_immediate("keystroke,print")
         data = self._socket.receive().decode("utf8")
         self.print_keystroke(data)
 
+
+
     def keystroke_clear(self):
         self.text_field.configure(state="normal")
         self.text_field.delete("1.0", tk.END)
         self.text_field.configure(state="disable")
+
--- code/client/src/frames/manager.py
+++ code/client/src/frames/manager.py
@@ -1,3 +1,4 @@
+from tkinter.constants import HORIZONTAL, VERTICAL
 from src.mysocket import MySocket
 from tkinter import ttk
 from threading import Thread
@@ -5,6 +6,9 @@ import tkinter as tk
 import src.utils as util
 import src.themecolors as THEMECOLOR
 import multiprocessing as mp
+
+
+
 
 
 class Manager(tk.Frame):
@@ -15,42 +19,75 @@ class Manager(tk.Frame):
         self._socket = MySocket.getInstance()
         self._inputbox = None
 
+
+
     def clean_activity(self):
         pass
 
+
+
     def create_widgets(self):
+        self.vertical_pane = ttk.PanedWindow(self, orient=VERTICAL, height=720)
+        self.vertical_pane.grid(row=0, column=0, sticky="nsew")
+        self.horizontal_pane = ttk.PanedWindow(self.vertical_pane, orient=HORIZONTAL, width=1024)
+        self.vertical_pane.add(self.horizontal_pane)
+        self.button_frame = ttk.Labelframe(self.horizontal_pane, text="My Button")
+        self.button_frame.columnconfigure(1, weight=2)
+        self.horizontal_pane.add(self.button_frame, weight=1)
+        self.console_frame = ttk.Labelframe(self.horizontal_pane, text="Console")
+        self.console_frame.columnconfigure(0, weight=10)
+        self.console_frame.rowconfigure(0, weight=1)
+        self.horizontal_pane.add(self.console_frame, weight=1)
+
         # Prompt the inputbox
         # User will input the application or process id they want to kill
         self.btn_kill = tk.Button(
-            self, text="Kill", command=self.kill, width=10, height=2)
-        self.btn_kill.grid(row=0, column=0, sticky=tk.N, padx=10, pady=10)
+            self.button_frame, text="Kill", command=self.kill, width=10, height=2)
+        self.btn_kill.grid(row=0, column=0, sticky=tk.N,
+                           padx=10, pady=5, rowspan=2)
+
         # Refresh and show running process or application from the server
         self.btn_view = tk.Button(
-            self, text="Xem", command=self.view_async, width=10, height=2)
-        self.btn_view.grid(row=0, column=1, sticky=tk.N, padx=10, pady=10)
+            self.button_frame, text="Xem", command=self.view_async, width=10, height=2)
+        self.btn_view.grid(row=2, column=0, sticky=tk.N,
+                           padx=10, pady=5, rowspan=2)
+
         # Clear the running process or application table
         self.btn_clear = tk.Button(
-            self, text="Xóa", command=self.clear, width=10, height=2)
-        self.btn_clear.grid(row=0, column=2, sticky=tk.N, padx=10, pady=10)
+            self.button_frame, text="Xóa", command=self.clear, width=10, height=2)
+        self.btn_clear.grid(row=4, column=0, sticky=tk.N,
+                           padx=10, pady=5, rowspan=2)
+
         # Similar to btn_kill, but this will take the name of the application and start it instead
         self.btn_start = tk.Button(
-            self, text="Start", command=self.start, width=10, height=2)
-        self.btn_start.grid(row=0, column=3, sticky=tk.N, padx=10, pady=10)
+            self.button_frame, text="Start", command=self.start, width=10, height=2)
+        self.btn_start.grid(row=6, column=0, sticky=tk.N,
+                           padx=10, pady=5, rowspan=2)
+
         # Display info of running process or application from the server
         cols = ("Name", "ID", "Count thread")
-        self.table = ttk.Treeview(self, columns=cols, show="headings")
-        self.table.grid(row=1, column=0, sticky=tk.N,
-                        padx=10, pady=10, columnspan=4)
+        self.table = ttk.Treeview(self.console_frame, columns=cols, show="headings")
+        self.table.grid(row=0, column=0, sticky=tk.N + tk.W + tk.E + tk.S)
+
+
+
         for col in cols:
             self.table.heading(col, text=col)
+
         for i in range(10):
             self.table.insert("", "end", values=("_", "_", "_"))
+
+
 
     def view_async(self):
         Thread(target=self.view, args=()).start()
 
+
+
     def exec_command_async(self, cmd, act):
         Thread(target=self.exec_command, args=(cmd, act)).start()
+
+
 
     def populate_data(self, data):
         # for testing data will be in 2d list
@@ -64,39 +101,54 @@ class Manager(tk.Frame):
             self.table.insert("", "end", values=(
                 name_process, id_process, count_thread))
 
+
+
     def clear(self):
         # clear data in the tabel before updating
         for rowid in self.table.get_children():
             self.table.delete(rowid)
 
+
+
     def kill(self):
         if self._inputbox != None:
             self.reset_inputbox()
             return
+
         self._inputbox = util.inputbox(
             tk.Toplevel(self), tl=self._type, cmd="kill")
+
         # binding...
         self._inputbox.btn_get["command"] = lambda: self.exec_command_async(
             cmd=self._type, act="kill")
         self._inputbox.bind(
             "<Destroy>", lambda e: self.reset_inputbox())
         self._inputbox.mainloop()
+
         exit
+
+
 
     def view(self):
         self._socket._isconnected = self._socket.send_immediate(
             self._type + ',view')
+
         if not self._socket._isconnected:
             return
+
         data = self._socket.receive().decode("utf8")
         self.populate_data(data=data)
+
+
 
     def start(self):
         if self._inputbox != None:
             self.reset_inputbox()
             return
+
         self._inputbox = util.inputbox(
             tk.Toplevel(self), tl=self._type, cmd="start")
+
         # binding...
         self._inputbox.btn_get["command"] = lambda: self.exec_command_async(
             cmd=self._type, act="start")
@@ -105,19 +157,26 @@ class Manager(tk.Frame):
         self._inputbox.mainloop()
         exit
 
+
+
     def exec_command(self, cmd, act):
         target = self._inputbox.getvalue()
         # Send command to the server
         self._socket._isconncted = self._socket.send_immediate(
             ','.join([cmd, act, target]))
         self._inputbox.clear()
+
         if not self._socket._isconnected:
             return
+
         # Get response from server
         response = self._socket._sock.recv(32).decode("utf8")
         util.messagebox(title=cmd, msg=response,
                         type="info" if response == "SUCCESS" else "error")
 
+
+
     def reset_inputbox(self):
         self._inputbox.killbox()
         self._inputbox = None
+
